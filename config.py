7886677af2ae76f0be253d489b59f94b8b9cad64
Aleksandr Kutuzov <alleteam@gmail.com>
2021-01-31 17:04:04 +0300

    Initial version

---
+++ config.py
@@ -0,0 +1,6 @@
+PRESETS = {
+    'Night': {'temperature': 1700, 'brightness': 1},
+    'Evening': {'temperature': 2800, 'brightness': 30},
+    'Half': {'temperature': 4000, 'brightness': 50},
+    'Day': {'temperature': 4200, 'brightness': 100},
+}
